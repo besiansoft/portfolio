6f8e6fbaa6c73650a1f3e76dfe2806b7b475a650
besian <besian@townsquaredigital.com>
2020-06-30 00:46:23 -0400

    update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,19 +1,23 @@
+import json
 import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
-import mimetypes 
+import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-build_bucket = s3.Bucket('portfoliobuild.bksoftaws.com')
-
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-portfolio_bucket_list = s3.Bucket('bksoftaws.com')
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket_list.upload_fileobj(obj, nm,
-        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket_list.Object(nm).Acl().put(ACL='public-read')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    build_bucket = s3.Bucket('portfoliobuild.bksoftaws.com')
+    
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket_list = s3.Bucket('bksoftaws.com')
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket_list.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket_list.Object(nm).Acl().put(ACL='public-read')
+        # TODO implement
+    return 'hello from Lambda'
